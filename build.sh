334e2a8918bc138a7625156f735cddc2a50205a7
martin roos <kulminaator@gmail.com>
2026-03-05 23:30:14 +0200

    improving buildh.sh in preparation for ghcr

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-docker build -t docker-4-mistral-vibe:latest .
+CURRENT_MISTRAL_VERSION=2.3.0
+echo "Building with mistral claiming to be $CURRENT_MISTRAL_VERSION"
+
+docker build -t docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:$CURRENT_MISTRAL_VERSION .
+
