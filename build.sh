66749af9f30eef7275fe8917eae1e9df31a25c7d
martin roos <kulminaator@gmail.com>
2026-03-12 20:21:37 +0200

    force rebuild of image every time in build.sh , bump to version 2.4.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
-CURRENT_MISTRAL_VERSION=2.4.1
+CURRENT_MISTRAL_VERSION=2.4.2
 echo "Building with mistral claiming to be $CURRENT_MISTRAL_VERSION"
 
-docker build -t docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:$CURRENT_MISTRAL_VERSION .
+docker build --no-cache -t docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:$CURRENT_MISTRAL_VERSION .
 
