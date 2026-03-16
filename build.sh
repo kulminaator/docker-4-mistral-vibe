a30be338621cead014bea07bfb4e368831153752
martin roos <kulminaator@gmail.com>
2026-03-16 22:34:51 +0200

    Bump to version 2.5.0, remove test conflict with my home .vibe folder

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-CURRENT_MISTRAL_VERSION=2.4.2
+CURRENT_MISTRAL_VERSION=2.5.0
 echo "Building with mistral claiming to be $CURRENT_MISTRAL_VERSION"
 
 docker build --no-cache -t docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:latest -t ghcr.io/kulminaator/docker-4-mistral-vibe:$CURRENT_MISTRAL_VERSION .
--- tests/test5_good_sample_from_readme.sh
+++ tests/test5_good_sample_from_readme.sh
@@ -1,6 +1,9 @@
+mkdir /tmp/tempvibedir
+
 docker  \
     run -e LOCAL_UID=$(id -u) -e LOCAL_GID=$(id -g) -e MISTRAL_API_KEY=$MISTRAL_API_KEY \
-    -v $(pwd):/project -v ~/.vibe:/vibehome \
+    -v $(pwd):/project -v /tmp/tempvibedir:/vibehome \
     --network host \
     -it --rm ghcr.io/kulminaator/docker-4-mistral-vibe:latest vibe
 
+rm -rf /tmp/tempvibedir
