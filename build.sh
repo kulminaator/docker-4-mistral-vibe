7d0468e29494efc61acd9a295af9b18b96cc704f
martin roos <kulminaator@gmail.com>
2026-03-31 21:17:23 +0300

    Updated mistral vibe to 2.7.1

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-CURRENT_MISTRAL_VERSION=2.7.0
+CURRENT_MISTRAL_VERSION=2.7.1
 echo "Building with mistral claiming to be $CURRENT_MISTRAL_VERSION"
 
 docker build --no-cache --build-arg MISTRAL_VIBE_VERSION=$CURRENT_MISTRAL_VERSION \
