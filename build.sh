0ff8dcbd5cc1ffbbc862ea0d5ab922a5fb1712ae
martin roos <kulminaator@gmail.com>
2026-04-04 12:33:19 +0300

    omit the no cache from docker build, now that we operate by git tags it is not needed anymore

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 CURRENT_MISTRAL_VERSION=2.7.3
 echo "Building with mistral claiming to be $CURRENT_MISTRAL_VERSION"
 
-docker build --no-cache --build-arg MISTRAL_VIBE_VERSION=$CURRENT_MISTRAL_VERSION \
+docker build --build-arg MISTRAL_VIBE_VERSION=$CURRENT_MISTRAL_VERSION \
        	-t docker-4-mistral-vibe:latest \
 	-t ghcr.io/kulminaator/docker-4-mistral-vibe:latest \
 	-t ghcr.io/kulminaator/docker-4-mistral-vibe:$CURRENT_MISTRAL_VERSION .
